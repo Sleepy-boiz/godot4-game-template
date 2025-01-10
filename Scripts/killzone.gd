dc397129877719b7913229ff92417151101a16a6
Sleepyboi <mason.d.klein23@gmail.com>
2025-01-10 13:12:38 -0600

    Fixed
    added
    -Death again
    -End kinda

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -2,18 +2,12 @@ extends Area2D
 
 @onready var timer = $Timer
 #drag timer node intop code and hold ctrl and adds a variable :3
-@onready var death: AnimationPlayer = %"death animation"
-var PLAYER = preload("res://Scenes/player.tscn")
+@onready var death_pos: AnimationPlayer = %DeathPos
 
 func _on_body_entered(body):
 	timer.start()
-	#Engine.time_scale = 0.5
-	#
+	Engine.time_scale = 0.5
 func _on_timer_timeout():
-	death.play("death")
-	await (death)
-	PLAYER.global_position = get_node("%SpawnPosition").global_position
-
-	#get_tree().reload_current_scene()
-	#Engine.time_scale = 1
+	death_pos.play("Death")
+	Engine.time_scale = 1
 ##just relods the current scene but get_tree
